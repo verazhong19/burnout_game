eea2b3d770b151d20eb3d46ff96186fb0ddbf3c0
Vera Zhong <verazhong@10-18-193-223.dynapool.wireless.nyu.edu>
2023-03-02 21:55:00 -0500

    initial task assignment system added

--- scripts/healthBar.gd
+++ scripts/healthBar.gd
@@ -6,12 +6,18 @@ var currentTaskTime
 var newTaskTime
 var timer = 1
 onready var damage_button = $damage_button
+onready var task = get_node("/root/TaskManager")
 var taskTimer = 1
 var taskActive =  false
+onready var taskLabel= $Task
+onready var assigner = $AssignedBy
+onready var message = $Message
+onready var due = $Due
 
 
 func _ready():
 	damage_button.connect("pressed", self, "_button_pressed")
+	print(task.availableTasks[0]["task"])
 
 	
 func _process(delta):
@@ -25,7 +31,7 @@ func _process(delta):
 			timer = 1
 	
 	if currentHealth == 0:
-		get_tree().change_scene("res://GameOver.tscn")
+		get_tree().change_scene("res://scenes/GameOver.tscn")
 	
 	if taskActive == true:	
 		taskTimer -= delta
@@ -36,6 +42,9 @@ func _process(delta):
 
 func _button_pressed():
 	setDamage()
+	taskLabel.text = task.availableTasks[0]["task"]
+	assigner.text = task.availableTasks[0]["assignedBy"]
+	message.text = task.availableTasks[0]["message"]
 
 
 	
@@ -55,5 +64,3 @@ func taskInProgress():
 	currentTaskTime = get_node("ProgressBar").get_value()
 	newTaskTime = get_node("ProgressBar").set_value(currentTaskTime + 1)
 
-
-	
---
+++ scripts/taskManager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var availableTasks = [{"task": "code", "assignedBy": "manager", "timeToComplete": 5, "message": "Hey, we need to refactor the code base for the inventory system. Can you spend some time to fix the bugs from the last version?"}, 
+	{"task": "model", "assignedBy": "CEO", "timeToComplete": 15, "message": "Hey, we need some 3D assets to build out the environment. Can you do that for me?"}, 
+	{"task": "git", "assignedBy": "coworker", "timeToComplete": 3, "message": "I need the updated version of the code. Can you push your changes to LitHub?"},
+	{"task": "art", "assignedBy": "manager", "timeToComplete": 5, "message": "I want to test out a new mechanic and need a character sprite. Can u draw one for me?"},
+	{"task": "engine", "assignedBy": "manager", "timeToComplete": 5, "message": "Need some new level designs. Mind prototyping something in Anarky?"},]
