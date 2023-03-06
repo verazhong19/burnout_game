b3d0213dd6357bced6ece17b33ee3f56837d3b68
Vera Zhong <verazhong@10-18-193-223.dynapool.wireless.nyu.edu>
2023-03-06 15:47:50 -0500

    lol

---
+++ scenes/do_task.gd
@@ -0,0 +1,22 @@
+extends Control
+onready var code_button = $code
+onready var git_button = $git
+onready var art_button = $art
+onready var model_button = $model
+onready var engine_button = $engine
+onready var task = $"../Task"
+
+func _ready():
+	code_button.connect("pressed", self, "_button_pressed",[code_button])
+	git_button.connect("pressed", self, "_button_pressed", [git_button])
+	art_button.connect("pressed", self, "_button_pressed", [art_button])
+	model_button.connect("pressed", self, "_button_pressed", [model_button])
+	engine_button.connect("pressed", self, "_button_pressed", [engine_button])
+	
+func _button_pressed(button):
+#	checkTask()
+#	print(button.text)
+	if(task.text == button.text):
+		print("correct task")
+	else:
+		print("wrong task")
